8e0a3595a09a817c5f19e68a4c599dfa7efb5488
Nam <epoche02@naver.com>
2022-08-07 16:19:21 +0900

    add service function interceptor

--- myyk/src/main/java/myyk/util/interceptor/ServiceFunctionInterceptor.java
+++ myyk/src/main/java/myyk/util/interceptor/ServiceFunctionInterceptor.java
@@ -6,13 +6,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import myyk.util.annotation.ServiceFunction;
+
 public class ServiceFunctionInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		
-		handler = (HandlerMethod) handler;  
+		HandlerMethod handlerMethod = (HandlerMethod) handler;  
+		
+		ServiceFunction function = handlerMethod.getBeanType().getAnnotation(ServiceFunction.class);
+		
+		request.setAttribute("category", function.value());
 		
 		return super.preHandle(request, response, handler);
 	}
